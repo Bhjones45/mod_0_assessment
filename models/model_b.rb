e467f45326173e9824f463f7e27347fcfdf9a136
Brett Jones <bhjones4545@gmail.com>
2021-04-02 15:31:22 -0600

    Add model developer using hash

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer = {
+  "hair_color" => "Blonde",
+  "eye_color" => "Blue",
+  "height_in_feet" => 6,
+  "age" => 26,
+  "is_goofy" => true,
+  "is_talkative" => false,
+  "hobbies" => ["Swimming", "Hiking", "Bike riding"],
+  "transportation" => ["foot", "bike", "car"]
+}

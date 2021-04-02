3dcb2241c4cd610e9d3827caae60b3178176924b
Brett Jones <bhjones4545@gmail.com>
2021-04-02 15:21:45 -0600

    Add model dinosaur using hash

---
+++ models/model_a.rb
@@ -0,0 +1,11 @@
+dinosaur = {
+  "name" => "Tyrannosaurus",
+  "era_lived_in" => "Cretaceous",
+  "average_height_in_feet" => 16,
+  "num_neck_vertebrae" => 10,
+  "is_extinct" => true,
+  "is_scary" => true,
+  "diet" => ["Edmontosaurus", "Triceratops"],
+  "habitat" => ["humid", "open forests", "semi-tropical environment", "coastal forested swamps"]
+  
+}

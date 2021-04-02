6b85ed59ab65100e9c4d0db61cc370cc5cfc5199
Brett Jones <bhjones4545@gmail.com>
2021-04-02 15:00:40 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,15 @@
+movie = "Justice League"
+is_entertaining = true
+
+movie = "Godzilla vs. Kong"
+num_4 = 8
+num_6 = 12
+product = num_4 * num_6
+#  wasn't sure if this was needed or if im getting ahead of myself
+puts product
+
+favorite_foods = ["Thai", "Chicken", "Chips"]
+favorite_foods.shift
+
+puts favorite_foods
+puts movie.size
